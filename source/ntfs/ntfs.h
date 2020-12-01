9768433c3032ae8348f5d1e30fbd153dd024b8a2
Rhys Koedijk <rhys@koedijk.co.nz>
2020-12-02 01:04:39 +1300

    Stubbed ntfs devoptab

--- source/ntfs/ntfs.h
+++ source/ntfs/ntfs.h
@@ -14,22 +14,58 @@
 #ifndef __NTFS_H__
 #define __NTFS_H__
 
+#include <switch.h>
+
+#include "ntfs-3g/config.h"
+#include "ntfs-3g/types.h"
+#include "ntfs-3g/device.h"
+#include "ntfs-3g/volume.h"
+
 /* NTFS errno values */
-#define ENOPART                         3000 /* No partition was found */
-#define EINVALPART                      3001 /* Specified partition is invalid or not supported */
-#define EDIRTY                          3002 /* Volume is dirty and NTFS_RECOVER was not specified during mount */
-#define EHIBERNATED                     3003 /* Volume is hibernated and NTFS_IGNORE_HIBERFILE was not specified during mount */
+#define ENOPART                         	3000 /* No partition was found */
+#define EINVALPART                      	3001 /* Specified partition is invalid or not supported */
+#define EDIRTY                          	3002 /* Volume is dirty and NTFS_RECOVER was not specified during mount */
+#define EHIBERNATED                     	3003 /* Volume is hibernated and NTFS_IGNORE_HIBERFILE was not specified during mount */
 
 /* NTFS mount flags */
-#define NTFS_DEFAULT                    0x00000000 /* Standard mount, expects a clean, non-hibernated volume */
-#define NTFS_SHOW_HIDDEN_FILES          0x00000001 /* Display hidden files when enumerating directories */
-#define NTFS_SHOW_SYSTEM_FILES          0x00000002 /* Display system files when enumerating directories */
-#define NTFS_UPDATE_ACCESS_TIMES        0x00000004 /* Update file and directory access times */
-#define NTFS_RECOVER                    0x00000008 /* Reset $LogFile if dirty (i.e. from unclean disconnect) */
-#define NTFS_IGNORE_HIBERFILE           0x00000010 /* Mount even if volume is hibernated */
-#define NTFS_READ_ONLY                  0x00000020 /* Mount in read only mode */
-#define NTFS_IGNORE_CASE                0x00000040 /* Ignore case sensitivity. Everything must be and will be provided in lowercase. */
-#define NTFS_SU                         NTFS_SHOW_HIDDEN_FILES | NTFS_SHOW_SYSTEM_FILES
-#define NTFS_FORCE                      NTFS_RECOVER | NTFS_IGNORE_HIBERFILE
+#define NTFS_DEFAULT                    	0x00000000 /* Standard mount, expects a clean, non-hibernated volume */
+#define NTFS_SHOW_HIDDEN_FILES          	0x00000001 /* Display hidden files when enumerating directories */
+#define NTFS_SHOW_SYSTEM_FILES          	0x00000002 /* Display system files when enumerating directories */
+#define NTFS_UPDATE_ACCESS_TIMES        	0x00000004 /* Update file and directory access times */
+#define NTFS_RECOVER                    	0x00000008 /* Reset $LogFile if dirty (i.e. from unclean disconnect) */
+#define NTFS_IGNORE_HIBERFILE           	0x00000010 /* Mount even if volume is hibernated */
+#define NTFS_READ_ONLY                  	0x00000020 /* Mount in read only mode */
+#define NTFS_IGNORE_CASE               	 	0x00000040 /* Ignore case sensitivity. Everything must be and will be provided in lowercase. */
+#define NTFS_SU                         	NTFS_SHOW_HIDDEN_FILES | NTFS_SHOW_SYSTEM_FILES
+#define NTFS_FORCE                      	NTFS_RECOVER | NTFS_IGNORE_HIBERFILE
+
+/*
+ * ntfs_atime_t - File access time update strategies
+ */
+typedef enum {
+    ATIME_ENABLED,                          /* Update access times */
+    ATIME_DISABLED                          /* Don't update access times */
+} ntfs_atime_t;
+
+/*
+ * ntfs_vd - NTFS volume descriptor
+ */
+typedef struct _ntfs_vd {
+    struct ntfs_device *dev;                /* NTFS device handle */
+    ntfs_volume *vol;                       /* NTFS volume handle */
+    s64 id;                                 /* Filesystem id */
+    u32 flags;                              /* Mount flags */
+    char name[128];                         /* Volume name (cached) */
+    u16 uid;                                /* User id for entry creation */
+    u16 gid;                                /* Group id for entry creation */
+    u16 fmask;                              /* Unix style permission mask for file creation */
+    u16 dmask;                              /* Unix style permission mask for directory creation */
+    ntfs_atime_t atime;                     /* Entry access time update strategy */
+    bool showHiddenFiles;                   /* If true, show hidden files when enumerating directories */
+    bool showSystemFiles;                   /* If true, show system files when enumerating directories */
+    ntfs_inode *cwd_ni;                     /* Current directory */
+} ntfs_vd;
+
+typedef ntfs_vd NTFS;
 
 #endif /* __NTFS_H__ */
---
+++ source/ntfs/ntfs_dev.c
@@ -0,0 +1,279 @@
+/*
+ * ntfs_dev.c
+ *
+ * Copyright (c) 2020, DarkMatterCore <pabloacurielz@gmail.com>.
+ * Copyright (c) 2020, XorTroll.
+ * Copyright (c) 2020, Rhys Koedijk.
+ *
+ * This file is part of libusbhsfs (https://github.com/DarkMatterCore/libusbhsfs).
+ * This file is based on work from libntfs-wii (https://github.com/rhyskoedijk/libntfs-wii).
+ */
+
+#include "ntfs_dev.h"
+
+static int       ntfsdev_open(struct _reent *r, void *fileStruct, const char *path, int flags, int mode);
+static int       ntfsdev_close(struct _reent *r, void *fd);
+static ssize_t   ntfsdev_write(struct _reent *r, void *fd, const char *ptr, size_t len);
+static ssize_t   ntfsdev_read(struct _reent *r, void *fd, char *ptr, size_t len);
+static off_t     ntfsdev_seek(struct _reent *r, void *fd, off_t pos, int dir);
+static int       ntfsdev_fstat(struct _reent *r, void *fd, struct stat *st);
+static int       ntfsdev_stat(struct _reent *r, const char *file, struct stat *st);
+static int       ntfsdev_link(struct _reent *r, const char *existing, const char *newLink);
+static int       ntfsdev_unlink(struct _reent *r, const char *name);
+static int       ntfsdev_chdir(struct _reent *r, const char *name);
+static int       ntfsdev_rename(struct _reent *r, const char *oldName, const char *newName);
+static int       ntfsdev_mkdir(struct _reent *r, const char *path, int mode);
+static DIR_ITER* ntfsdev_diropen(struct _reent *r, DIR_ITER *dirState, const char *path);
+static int       ntfsdev_dirreset(struct _reent *r, DIR_ITER *dirState);
+static int       ntfsdev_dirnext(struct _reent *r, DIR_ITER *dirState, char *filename, struct stat *filestat);
+static int       ntfsdev_dirclose(struct _reent *r, DIR_ITER *dirState);
+static int       ntfsdev_statvfs(struct _reent *r, const char *path, struct statvfs *buf);
+static int       ntfsdev_ftruncate(struct _reent *r, void *fd, off_t len);
+static int       ntfsdev_fsync(struct _reent *r, void *fd);
+static int       ntfsdev_chmod(struct _reent *r, const char *path, mode_t mode);
+static int       ntfsdev_fchmod(struct _reent *r, void *fd, mode_t mode);
+static int       ntfsdev_rmdir(struct _reent *r, const char *name);
+static int       ntfsdev_utimes(struct _reent *r, const char *filename, const struct timeval times[2]);
+
+static const devoptab_t ntfsdev_devoptab = {
+    .name         = NULL,
+    .structSize   = sizeof(ntfs_file_state),
+    .open_r       = ntfsdev_open,
+    .close_r      = ntfsdev_close,
+    .write_r      = ntfsdev_write,
+    .read_r       = ntfsdev_read,
+    .seek_r       = ntfsdev_seek,
+    .fstat_r      = ntfsdev_fstat,
+    .stat_r       = ntfsdev_stat,
+    .link_r       = ntfsdev_link,
+    .unlink_r     = ntfsdev_unlink,
+    .chdir_r      = ntfsdev_chdir,
+    .rename_r     = ntfsdev_rename,
+    .mkdir_r      = ntfsdev_mkdir,
+    .dirStateSize = sizeof(ntfs_dir_state),
+    .diropen_r    = ntfsdev_diropen,
+    .dirreset_r   = ntfsdev_dirreset,
+    .dirnext_r    = ntfsdev_dirnext,
+    .dirclose_r   = ntfsdev_dirclose,
+    .statvfs_r    = ntfsdev_statvfs,
+    .ftruncate_r  = ntfsdev_ftruncate,
+    .fsync_r      = ntfsdev_fsync,
+    .deviceData   = NULL,
+    .chmod_r      = ntfsdev_chmod,
+    .fchmod_r     = ntfsdev_fchmod,
+    .rmdir_r      = ntfsdev_rmdir,
+    .lstat_r      = ntfsdev_stat,
+    .utimes_r     = ntfsdev_utimes
+};
+
+const devoptab_t *ntfsdev_get_devoptab()
+{
+    return &ntfsdev_devoptab;
+}
+
+
+int ntfsdev_open (struct _reent *r, void *fileStruct, const char *path, int flags, int mode)
+{
+    ntfs_log_trace("fileStruct %p, path %s, flags %i, mode %i\n", (void *) fileStruct, path, flags, mode);
+
+    // TODO: This...
+
+    return 0;
+}
+
+int ntfsdev_close (struct _reent *r, void *fd)
+{
+    ntfs_log_trace("fd %p\n", fd);
+
+    // TODO: This...
+
+    return 0;
+}
+
+ssize_t ntfsdev_write (struct _reent *r, void *fd, const char *ptr, size_t len)
+{
+    ntfs_log_trace("fd %p, ptr %p, len %lu\n", fd, ptr, len);
+
+    // TODO: This...
+
+    return 0;
+}
+
+ssize_t ntfsdev_read (struct _reent *r, void *fd, char *ptr, size_t len)
+{
+    ntfs_log_trace("fd %p, ptr %p, len %lu\n", fd, ptr, len);
+
+    // TODO: This...
+
+    return 0;
+}
+
+off_t ntfsdev_seek (struct _reent *r, void *fd, off_t pos, int dir)
+{
+    ntfs_log_trace("fd %p, pos %li, dir %i\n", fd, pos, dir);
+
+    // TODO: This...
+
+    return 0;
+}
+int ntfsdev_fstat (struct _reent *r, void *fd, struct stat *st)
+{
+    ntfs_log_trace("fd %p\n", fd);
+
+    // TODO: This...
+
+    return 0;
+}
+
+int ntfsdev_stat (struct _reent *r, const char *path, struct stat *st)
+{
+    ntfs_log_trace("path %s, st %p\n", path, st);
+
+    // TODO: This...
+
+    return 0;
+}
+
+int ntfsdev_link (struct _reent *r, const char *existing, const char *newLink)
+{
+    ntfs_log_trace("existing %s, newLink %s\n", existing, newLink);
+
+    // TODO: This...
+
+    return 0;
+}
+
+int ntfsdev_unlink (struct _reent *r, const char *name)
+{
+    ntfs_log_trace("name %s\n", name);
+
+    // TODO: This...
+
+    return 0;
+}
+
+int ntfsdev_chdir (struct _reent *r, const char *name)
+{
+    ntfs_log_trace("name %s\n", name);
+
+    // TODO: This...
+
+    return 0;
+}
+
+int ntfsdev_rename (struct _reent *r, const char *oldName, const char *newName)
+{
+    ntfs_log_trace("oldName %s, newName %s\n", oldName, newName);
+
+    // TODO: This...
+
+    return 0;
+}
+
+int ntfsdev_mkdir (struct _reent *r, const char *path, int mode)
+{
+    ntfs_log_trace("path %s, mode %i\n", path, mode);
+
+    // TODO: This...
+
+    return 0;
+}
+
+DIR_ITER *ntfsdev_diropen (struct _reent *r, DIR_ITER *dirState, const char *path)
+{
+    ntfs_log_trace("dirState %p, path %s\n", dirState, path);
+
+    // TODO: This...
+
+    return dirState;
+}
+
+int ntfsdev_dirreset (struct _reent *r, DIR_ITER *dirState)
+{
+    ntfs_log_trace("dirState %p\n", dirState);
+
+    // TODO: This...
+
+    return 0;
+}
+
+int ntfsdev_dirnext (struct _reent *r, DIR_ITER *dirState, char *filename, struct stat *filestat)
+{
+    ntfs_log_trace("dirState %p, filename %p, filestat %p\n", dirState, filename, filestat);
+
+    // TODO: This...
+
+    return 0;
+}
+
+int ntfsdev_dirclose (struct _reent *r, DIR_ITER *dirState)
+{
+    ntfs_log_trace("dirState %p\n", dirState);
+
+    // TODO: This...
+
+    return 0;
+}
+
+int ntfsdev_statvfs (struct _reent *r, const char *path, struct statvfs *buf)
+{
+    ntfs_log_trace("path %s, buf %p\n", path, buf);
+
+    // TODO: This...
+
+    return 0;
+}
+
+int ntfsdev_ftruncate (struct _reent *r, void *fd, off_t len)
+{
+    ntfs_log_trace("fd %p, len %lu\n", fd, (u64) len);
+
+    // TODO: This...
+
+    return 0;
+}
+
+int ntfsdev_fsync (struct _reent *r, void *fd)
+{
+    ntfs_log_trace("fd %p\n", fd);
+
+    // TODO: This...
+
+    return 0;
+}
+
+int ntfsdev_chmod (struct _reent *r, const char *path, mode_t mode)
+{
+    ntfs_log_trace("path %s, mode %i\n", path, mode);
+
+    // TODO: This...
+
+    return 0;
+}
+
+int ntfsdev_fchmod (struct _reent *r, void *fd, mode_t mode)
+{
+    ntfs_log_trace("fd %p, mode %i\n", fd, mode);
+
+    // TODO: This...
+
+    return 0;
+}
+
+int ntfsdev_rmdir (struct _reent *r, const char *name)
+{
+    ntfs_log_trace("name %s\n", name);
+
+    // TODO: This...
+
+    return 0;
+}
+
+int ntfsdev_utimes (struct _reent *r, const char *filename, const struct timeval times[2])
+{
+    ntfs_log_trace("filename %s, time[0] %li, time[1] %li\n", filename, times[0].tv_sec, times[1].tv_sec);
+
+    // TODO: This...
+
+    return 0;
+}
